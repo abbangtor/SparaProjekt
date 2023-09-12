0b4acfb82412b43a2c95249d8acf75c6f4d8c429
abbcecek <cecilia.ek@abbgymnasiet.se>
2023-09-12 10:36:14 +0200

    minus poäng (Cecilia och Elvira)

--- Assets/ScoreCounter.cs
+++ Assets/ScoreCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,18 +11,43 @@ public class ScoreCounter : MonoBehaviour
     [SerializeField] private Text _score;
     [SerializeField] private string _tag;
 
+    List<string> trashList = new List<string>()
+    {
+        "Metal",
+        "Plastic",
+        "Paper",
+        "Compost",
+        "Pant"
+    };
+
     //[SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag(_tag))
+        
+
+        for (int i = 0; i < trashList.Count; i++)
         {
-            //collectionSoundEffect.Play();
-            collision.GetComponentInParent<SkräpUppplockning>().stuckTrash = false;
-            Destroy(collision.transform.parent.gameObject);
-            Destroy(collision.gameObject);
-            points++;
-            _score.text = "Poäng: " + points;
+            if (collision.gameObject.CompareTag(trashList[i]))
+            {
+                Debug.Log(collision.tag);
+
+                collision.GetComponentInParent<SkräpUppplockning>().stuckTrash = false;
+                Destroy(collision.transform.parent.gameObject);
+                Destroy(collision.gameObject);
+
+                if (collision.gameObject.CompareTag(_tag))
+                {
+                    points++;
+                }
+                else
+                {
+                    points--;
+                }
+
+                _score.text = "Poäng: " + points;
+
+            }
         }
     }
 }
