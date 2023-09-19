84dba50597b443eca24b94ff164bb2584d8e043b
abbangtor <angla.tornros@abbgymnasiet.se>
2023-09-19 09:13:57 +0200

    Created Basic Spawner
    Spawner that spawns a prefab at "SpawningPosition" (an empty game object), from a select list of prefabs

--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class TrashSpawner : MonoBehaviour
 {
+    // Variable we´re going to put our prefab into
     public GameObject objectToSpawn;
+    
+
+    public GameObject name1;
+    public GameObject name2;
+    public GameObject name3;
+   
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(objectToSpawn);
+        GameObject objectToSpawn = GameObject.Find("name1");
+
+        // Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+        Debug.Log(objectToSpawn.name);
+
     }
 
     // Update is called once per frame
