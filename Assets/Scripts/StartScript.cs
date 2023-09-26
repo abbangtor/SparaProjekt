32cf1d3ae8a2ecfbb132d6108e6f215031473afa
abbemirom <92145395+abbemirom@users.noreply.github.com>
2023-09-26 10:50:18 +0200

    Timer

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MainMenu : MonoBehaviour
+using UnityEngine.UI;
+public class StartScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeRemaining = 120;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public BoxCollider BowCollider;
+    public void OnStart()
     {
-        
+        // Starts the timer automatically
+        timerIsRunning = true;
+        BowCollider.enabled = true;
+        Game();
+      
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Game()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+                Game();
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
