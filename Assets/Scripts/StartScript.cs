9d4250352242ad2b05d4d988a7fdc6bccc2bb75d
abbelvtap <92145452+abbelvtap@users.noreply.github.com>
2023-09-27 10:17:17 +0200

    added sounds correct and wrong

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,4 +39,4 @@ public class StartScript : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
-}
+}*/
